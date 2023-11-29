acd3ece615bcb4106190f1f8ca9952cd8b90f932
Jose Manuel <jcmanueldj@gmail.com>
2023-11-29 20:48:15 -0300

    CSS inicio, propuestas

--- src/componentes/propuesta/GestionPropuesta.jsx
+++ src/componentes/propuesta/GestionPropuesta.jsx
@@ -54,51 +54,51 @@ const GestionPropuesta = () => {
 
     return (
         <div className='contenedor-propuesta'>
-        <h1 className='titulo'>PROPUESTA PARA SOCIOS</h1>
-        <h3>RECIBÍ UNA CAJA DE 6 VINOS EXCLUSIVOS
-        TODOS LOS MESES EN TU DOMICILIO</h3>
+            <h1 className='titulo'>Propuestas para socios</h1>
+            <p><span>Recibí una caja de 6 vinos exclusivos
+            todos los meses en tu domicilio</span> </p>
 
-        <div className="container1">
-            <div className="card">
-                <figure>
-                    <img src={botella}/>
-                </figure>
-                <div className="contenido1">
-                    <h3>ETIQUETAS EXCLUSIVAS</h3>
-                    <p>Etiquetas Originales de las mejores bodegas de Argentina y exclusivamente creadas para los socios del Club.</p>
+            <div className="container1">
+                <div className="card">
+                    <figure>
+                        <img src={botella}/>
+                    </figure>
+                    <div className="contenido1">
+                        <h3>ETIQUETAS EXCLUSIVAS</h3>
+                        <p>Etiquetas Originales de las mejores bodegas de Argentina y exclusivamente creadas para los socios del Club.</p>
+                    </div>
                 </div>
-            </div>
-            
-            <div className="card">
-                <figure>
-                    <img src={copa}/>
-                </figure>
-                <div className="contenido1">
-                    <h3>CATAS Y EVENTOS</h3>
-                <p>Se parte de Eventos exclusivos para nuestros socios. Degustaciones gratuitas, charlas con enólogos reconocidos y más eventos</p>
+                
+                <div className="card">
+                    <figure>
+                        <img src={copa}/>
+                    </figure>
+                    <div className="contenido1">
+                        <h3>CATAS Y EVENTOS</h3>
+                    <p>Se parte de Eventos exclusivos para nuestros socios. Degustaciones gratuitas, charlas con enólogos reconocidos y más eventos</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className="card">
-                <figure>
-                    <img src={camion}/>
-                </figure>
-                <div className="contenido1">
-                    <h3>BONIFICACIONES EN ENVIO</h3>
-                    <p>Recibí una caja de seis vinos mensualmente y aprovechá 50% de Descuento en el costo de envío.</p>
-                </div>
-            </div> 
-        </div>   
+                <div className="card">
+                    <figure>
+                        <img src={camion}/>
+                    </figure>
+                    <div className="contenido1">
+                        <h3>BONIFICACIONES EN ENVIO</h3>
+                        <p>Recibí una caja de seis vinos mensualmente y aprovechá 50% de Descuento en el costo de envío.</p>
+                    </div>
+                </div> 
+            </div>   
 
 
-        {/* Lista de Vinos */}
-        <ListaDeVinos
-            vinos={vinos}
-            completarVino={completarVino}
-            eliminarVino={eliminarVino}
-        />
-        {/* Formulario para agregar nuevos Vinos */}
-        <FormularioDeVinos agregarVino={agregarVino} />
+            {/* Lista de Vinos */}
+            <ListaDeVinos
+                vinos={vinos}
+                completarVino={completarVino}
+                eliminarVino={eliminarVino}
+            />
+            {/* Formulario para agregar nuevos Vinos */}
+            <FormularioDeVinos agregarVino={agregarVino} />
         </div>
     );
     };
