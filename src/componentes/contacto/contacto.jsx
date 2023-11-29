ab95e334d5902d8a845e4a4220e76206054e4285
Dasr85 <dsolorzano1985@gmail.com>
2023-11-29 20:49:55 -0300

    contacto

--- src/componentes/contacto/contacto.jsx
+++ src/componentes/contacto/contacto.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./contacto.css";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
+    name: "",
+    lname: "",
+    email: "",
+    phone: "",
+    message: "",
   });
 
   const [errors, setErrors] = useState({
-    name: '',
-    email: '',
-    message: ''
+    name: "",
+    lname: "",
+    email: "",
+    phone: "",
+    message: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
     setErrors({
       ...errors,
-      [name]: '' // Clear previous errors when user starts typing
+      [name]: "", // Clear previous errors when user starts typing
     });
   };
 
@@ -30,30 +35,47 @@ const ContactForm = () => {
     // Perform form validation
     let formValid = true;
 
-    // Name validation
-    if (formData.name.trim() === '') {
+    // Nombre validation
+    const nameRegex = /^[a-zA-ZÀ-ÿ\s]{1,20}$/;
+    if (!nameRegex.test(formData.name)) {
       setErrors({
         ...errors,
-        name: 'Please enter your name'
+        name: "Por favor ingrese su nombre, este campo solo admite letras",
       });
       formValid = false;
     }
-
+    // Apellido validation
+    const lnameRegex = /^[A-Za-z]{1,20}$/;
+    if (!lnameRegex.test(formData.lname)) {
+      setErrors({
+        ...errors,
+        lname: "Por favor ingrese su apellido, este campo solo admite letras",
+      });
+      formValid = false;
+    }
     // Email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(formData.email)) {
       setErrors({
         ...errors,
-        email: 'Please enter a valid email address'
+        email: "Por favor ingrese un email valido",
       });
       formValid = false;
     }
-
-    // Message validation
-    if (formData.message.trim() === '') {
+    // Telefono validation
+    const phoneRegex = /^\d{7,14}$/;
+    if (!phoneRegex.test(formData.phone)) {
       setErrors({
         ...errors,
-        message: 'Please enter a message'
+        phone: "Por favor ingrese un numero de telefono valido",
+      });
+      formValid = false;
+    }
+    // Message validation
+    if (formData.message.trim() === "") {
+      setErrors({
+        ...errors,
+        message: "Por favor ingrese un mensaje",
       });
       formValid = false;
     }
@@ -61,48 +83,70 @@ const ContactForm = () => {
     // If form is valid, you can proceed with submitting the data
     if (formValid) {
       // Perform your submit logic here
-      console.log('Form submitted:', formData);
+      console.log("Form submitted:", formData);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        <div className="error">{errors.name}</div>
-      </div>
+    <form className="formulario" onSubmit={handleSubmit}>
+      <div className="formulario__estilo">
+        <div className="form__texto">
+          <label className="form__txt" htmlFor="name">Nombre:</label>
+          <input className="form__input"
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <div className="error">{errors.name}</div>
+        </div>
+        <div className="form__texto">
+          <label className="form__txt" htmlFor="lname">Apellido:</label>
+          <input className="form__input"
+            type="text"
+            id="lname"
+            name="lname"
+            value={formData.lname}
+            onChange={handleChange}
+          />
+          <div className="error">{errors.lname}</div>
+        </div>
+        <div className="form__texto">
+          <label className="form__txt" htmlFor="email">Email:</label>
+          <input className="form__input"
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <div className="error">{errors.email}</div>
+        </div>
+        <div className="form__texto">
+          <label className="form__txt" htmlFor="phone">Telefono:</label>
+          <input className="form__input"
+            type="text"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+          <div className="error">{errors.phone}</div>
+        </div>
+        <div className="form__texto">
+          <label className="form__txt" htmlFor="message">Mensaje:</label>
+          <textarea className="form__input"
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <div className="error">{errors.message}</div>
+        </div>
 
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <div className="error">{errors.email}</div>
+        <button className="form__boton" type="submit">Submit</button>
       </div>
-
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-        />
-        <div className="error">{errors.message}</div>
-      </div>
-
-      <button type="submit">Submit</button>
     </form>
   );
 };
